fa17a3250d812a113bca7f4038dac7ded52e7b49
smsidki <sichy_helsinky@yahoo.com>
2018-12-12 22:11:18 +0700

    Stateless component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello World!</h1>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+    </div>
+  );
+};
 
 export default App;
