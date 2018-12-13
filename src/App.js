c6df7064aabab0fb7889768cd4edfeabef75c488
smsidki <sichy_helsinky@yahoo.com>
2018-12-13 09:39:24 +0700

    Default props

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React from 'react';
 import './App.css';
 import PropTypes from 'prop-types';
 
-const App = ({greeting}) => {
+const App = ({greeting, profession}) => {
   return (
     <div className="App">
       <h1>{greeting}</h1>
+      <p>{'I am a/an ' + profession}</p>
     </div>
   );
 };
 
 App.propTypes = {
-  name: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired,
+};
+
+App.defaultProps = {
+  profession: 'alien'
 };
 
 export default App;
