923059752f77470f6934c6f4dc954e3823c1b5f2
smsidki <sichy_helsinky@yahoo.com>
2018-12-13 10:16:47 +0700

    State

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import React from 'react';
 import './App.css';
 import PropTypes from 'prop-types';
 
-const App = ({greeting, profession}) => {
-  return (
-    <div className="App">
-      <h1>{greeting}</h1>
-      <p>{'I am a/an ' + profession}</p>
-    </div>
-  );
-};
+class App extends React.Component {
+
+  state = {
+    hobby: 'do nothing'
+  };
+
+  changeHobby = () => {
+    this.state.hobby = this.state.hobby === 'do nothing' ? 'run away from reality' : 'do nothing';
+    console.log(this.state.hobby);
+  };
+
+  render() {
+    console.log('rendered App.js');
+    const {hobby} = this.state;
+    const {greeting, profession, name} = this.props;
+    return (
+      <div className="App">
+        <h1>{greeting}</h1>
+        <p>{'I am ' + name + ', a/an ' + profession}</p>
+        <p>{'I like to ' + hobby}</p>
+        <button onClick={this.changeHobby}>Change hobby</button>
+      </div>
+    );
+  }
+}
 
 App.propTypes = {
   name: PropTypes.string.isRequired,
