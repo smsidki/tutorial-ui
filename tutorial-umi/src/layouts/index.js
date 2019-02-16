1c1c34994ad9632b4553ec02e690e97048e91245
smsidki <sichy_helsinky@yahoo.com>
2019-02-16 16:54:52 +0700

    # Lay01 - Change global layout

--- tutorial-umi/src/layouts/index.js
+++ tutorial-umi/src/layouts/index.js
@@ -1,11 +1,19 @@
 import styles from './index.css';
+import { Layout } from 'antd';
 
 function BasicLayout(props) {
   return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
-      { props.children }
-    </div>
+    <Layout className={styles.normal}>
+      <Layout.Header style={{backgroundColor: 'darkslateblue'}}>
+        <h1 style={{color: 'white'}}>Tutorial Umi</h1>
+      </Layout.Header>
+      <Layout.Content>
+        { props.children }
+      </Layout.Content>
+      <Layout.Footer className={styles.footer}>
+        {'Copyright \u00A9 2019'}
+      </Layout.Footer>
+    </Layout>
   );
 }
 
